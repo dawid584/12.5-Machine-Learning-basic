81ce380f9cd53945142aad3327b3962b1e8d96c0
Dawid Dymke <dawid584@yahoo.com>
2022-07-25 15:34:39 +0200

    Init project

--- samplefile.py
+++ samplefile.py
@@ -1,29 +1,12 @@
-#łączenie zbiorów
+sp = []
+shop = {"piekarnia" : ["chleb", "pączek", "bułki"] , "warzywniak" : ["marchew", "seler", "rukola"] }
+print("Lista zakupów:")
+amount = 0
+for shop_0 , product in shop.items():
+    amount = amount + len(product)
+    
+    sp = [  x.capitalize() for x in product ]
+     
+    print(f'Idę do {shop_0.title()}, kupuję tu tam : {sp} ' )
 
-shopping_A = {"pieczywo", "masło", "ser"}
-shopping_B = {"wędlina", "masło", "cytryna"}
-
-xc = shopping_A.union(shopping_B)
-
-print(xc)
-
-xd = shopping_A | shopping_B
-print(xd)
-
-# częśc wspólna
-xv = shopping_A & shopping_B
-print(xv)
-
-# iloczyn zbioróWarning
-xb =  shopping_A - shopping_B
-
-print(xb)
-
-
-dictionary = {"key_1" : ["azjatka" , "robot"], 123 : ["lizak" , "cukierek"]}
-print(dictionary)
-
-print(dictionary.keys())
-print(dictionary.values())
-print(dictionary[123])
-print(dictionary.get("key_1"))
+print(f"W sumie kupuję {amount} produktów.")
